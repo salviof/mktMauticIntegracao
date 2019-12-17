b9b15b33f3549d9b35eccb3f74bcbbba675a0adf
Salvio Furbino <salviof@gmail.com>
2019-12-17 01:06:24 -0300

    up

--- mauticClientRest/src/main/resources/carregaBanco.sh
+++
@@ -1,3 +0,0 @@
-source ./SBProjeto.prop
-mysqladmin -u root create $NOME_BANCO
-mysql -u root $NOME_BANCO < ./$NOME_BANCO.Homologacao.sql
--- mauticClientRest/src/main/resources/compilaBanco.sh
+++
@@ -1,2 +0,0 @@
-source ./SBProjeto.prop
-mysqldump -u root  $NOME_BANCO > $NOME_BANCO.Homologacao.sql
