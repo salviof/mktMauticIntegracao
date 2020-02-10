70b8a98bd3e604a8cda1d77ba8dc65870ccc4e75
Salvio Furbino <salviof@gmail.com>
2020-02-10 04:37:09 -0300

    Atualizacao automática

--- mauticClientRest/src/main/java/com/super_bits/Super_Bits/mktMauticIntegracao/regras_de_negocio_e_controller/FabConfigModuloMautic.java
+++ mauticClientRest/src/main/java/com/super_bits/Super_Bits/mktMauticIntegracao/regras_de_negocio_e_controller/FabConfigModuloMautic.java
@@ -6,8 +6,6 @@
 package com.super_bits.Super_Bits.mktMauticIntegracao.regras_de_negocio_e_controller;
 
 import com.super_bits.modulosSB.SBCore.ConfigGeral.arquivosConfiguracao.ItfFabConfigModulo;
-import com.super_bits.modulosSB.SBCore.ConfigGeral.arquivosConfiguracao.tipoModulos.integracaoOauth.FabPropriedadeModuloIntegracaoOauth;
-import com.super_bits.modulosSB.SBCore.ConfigGeral.arquivosConfiguracao.tipoModulos.integracaoOauth.InfoPropriedadeConfigOauth;
 
 /**
  *
@@ -15,13 +13,13 @@ import com.super_bits.modulosSB.SBCore.ConfigGeral.arquivosConfiguracao.tipoModu
  */
 public enum FabConfigModuloMautic implements ItfFabConfigModulo {
 
-    @InfoPropriedadeConfigOauth(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.SITE_SERVIDOR)
+    @InfoPropriedadeConfigRestIntegracao(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.SITE_SERVIDOR)
     URL_SERVIDOR_MAUTIC,
-    @InfoPropriedadeConfigOauth(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.SITE_CLIENTE)
+    @InfoPropriedadeConfigRestIntegracao(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.SITE_CLIENTE)
     URL_SERIVIDOR_CLIENTE,
-    @InfoPropriedadeConfigOauth(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.CHAVE_PUBLICA)
+    @InfoPropriedadeConfigRestIntegracao(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.CHAVE_PUBLICA)
     CHAVE_API_PUBLICA,
-    @InfoPropriedadeConfigOauth(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.CHAVE_PRIVADA)
+    @InfoPropriedadeConfigRestIntegracao(tipoPropriedade = FabPropriedadeModuloIntegracaoOauth.CHAVE_PRIVADA)
     CHAVE_API_PRIVADA;
 
     @Override
